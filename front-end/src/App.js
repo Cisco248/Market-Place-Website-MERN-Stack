099d694a46d6d20ac8d5c2361607678f7803a205
Lahiru Dilshan <129523905+Cisco248@users.noreply.github.com>
2024-01-26 16:10:50 +0530

    main file add

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import ElectronicPage from './pages/ElectronicPage'
+import {BidPage} from './pages/BidPage'
+import PostadPage from './pages/PostadPage'
+ 
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/electronic' element={<ElectronicPage />} />
+        <Route path='/product' element={<BidPage />} />
+        <Route path='/postad' element={<PostadPage/>} />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
